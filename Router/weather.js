3c9e73cc49d6e2331ef7221e29ec51bdf5e7466c
Ritesh Rai <riteshrai9236@gmail.com>
2024-08-30 23:15:15 +0530

    used node-cache for caching the responses coming from api

--- Router/weather.js
+++ Router/weather.js
@@ -1,4 +1,10 @@
 const express = require('express');
 
+const { getWeather } = require('../controllers/weatherController');
+
+const router = express.Router();
+
+// Define the route for getting weather information
+router.get('/weather', getWeather);
 
 module.exports = router;
